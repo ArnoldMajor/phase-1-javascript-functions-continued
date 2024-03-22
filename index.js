53129ed16c45688b5a16f68f75defc1281d28d82
ArnoldMajor <arnoldmwangakala@gmail.com>
2024-03-22 12:55:58 +0300

    Complete lab

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
-// code your solution here
+let activity;
+
+function saturdayFun(activity){
+    switch (activity) {
+    case undefined:
+        return 'This Saturday, I want to roller-skate!';
+        
+    default:
+        return `This Saturday, I want to ${activity}!`;
+    }    
+}
+
+function mondayWork(specified){
+    let defaultActivity = 'go to the office';
+    if (specified === undefined) {
+        return `This Monday, I will ${defaultActivity}.`;
+    }
+
+    else return `This Monday, I will ${specified}.`;
+}
+
+let highlight;
+
+function wrapAdjective(highlight){
+    return (function (value = 'special') {
+        return `You are ${highlight}${value}${highlight}!`
+    })
+
+}
